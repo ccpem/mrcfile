22af0d224104eca8b12d956b352b615169690f24
Colin Palmer <colin.palmer@stfc.ac.uk>
2021-02-18 00:31:44 +0000

    Fix warnings tests for different Python and numpy versions

--- tests/test_mrcobject.py
+++ tests/test_mrcobject.py
@@ -445,12 +445,18 @@ class MrcObjectTest(AssertRaisesRegexMixin, unittest.TestCase):
 
     def test_warning_for_stats_with_nan(self):
         data = np.arange(6, dtype=np.float32).reshape(3, 2)
-        data[1,1] = np.NaN
+        data[1,1] = np.nan
         with warnings.catch_warnings(record=True) as w:
+            # Hack for warnings bug in Python 2.7
+            #  See https://stackoverflow.com/q/56821539
+            if sys.version_info[0:2] == (2, 7):
+                from mrcfile import mrcobject
+                if hasattr(mrcobject, '__warningregistry__'):
+                    mrcobject.__warningregistry__.clear()
             warnings.simplefilter("always")
             self.mrcobject.set_data(data)
-            assert len(w) == 1
-            assert str(w[0].message) == "Data array contains NaN values"
+            assert len(w) >= 1, str(len(w)) + " warnings issued"
+            assert "Data array contains NaN values" in [str(i.message) for i in w]
             header = self.mrcobject.header
             assert np.isnan(header.dmin)
             assert np.isnan(header.dmax)
@@ -464,8 +470,8 @@ class MrcObjectTest(AssertRaisesRegexMixin, unittest.TestCase):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
             self.mrcobject.set_data(data)
-            assert len(w) > 1
-            assert str(w[0].message) == "Data array contains infinite values"
+            assert len(w) >= 1, str(len(w)) + " warnings issued"
+            assert "Data array contains infinite values" in [str(i.message) for i in w]
             header = self.mrcobject.header
             assert header.dmin == np.NINF
             assert header.dmax == np.inf
