6b38e8b7d30c8982098a69a8ffa41f26ecf07318
Colin Palmer <colin.palmer@stfc.ac.uk>
2022-07-02 22:25:50 +0100

    Add support and tests for Python 3.10 and numpy 1.23

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
